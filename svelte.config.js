87c478a6520f53f9b678b774716b736d965d8ddf
minikraken <maxime.huylebroeck@outlook.com>
2025-08-01 10:25:20 +0200

    fix base path since we use a custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -10,10 +10,8 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		} },
+		})
+	},
 	extensions: ['.svelte', '.svx']
 };
 
